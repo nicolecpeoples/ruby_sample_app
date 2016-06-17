dde2e2d7710b4823b02cb958edf7d55e4efc6974
Nicole Peoples <nicolecpeoples@gmail.com>
2016-06-16 19:06:34 -0700

    Finish Static Pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -5,15 +5,28 @@ RSpec.describe StaticPagesController, type: :controller do
   describe "GET #home" do
     it "returns http success" do
       get :home
-      expect(response).to have_http_status(:success)
+      assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+      assert_response :success
+      
     end
   end
 
   describe "GET #help" do
     it "returns http success" do
       get :help
-      expect(response).to have_http_status(:success)
+      assert_response :success
+      
     end
   end
+
+   describe "GET #about" do
+    it "returns http success" do
+      get :about
+      assert_response :success
+     
+    end
+  end
+
+
 
 end
